64022402465829539e801092c5199df2fe75620b
Pyry Pajunpää <pymapa@utu.fi>
2015-06-16 00:59:44 +0300

    täydennystä, päivitystä

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,10 @@
+var crypto = require('crypto');
+var mongoose = require('mongoose'),
+	User = mongoose.model('User');
+function hashPW(pwd) {
+	return crypto.createHash('sha256').update(pwd).
+		digest('base64').toString();
+};
 exports.signup = function(req, res) {
 	var user = new User({username:req.body.username});
 	user.set('hashed_password', hashPW(req.body.password));
@@ -11,6 +18,74 @@ exports.signup = function(req, res) {
 			req.session.username = user.username;
 			req.session.msg = 'Authenticated as ' + user.username;
 			res.redirect('/');
+		}
+	});
+};
+exports.login = function(req, res) {
+	User.findOne({username: req.body.username})
+	.exec(function(err, user) {
+		if (!user) {
+			err = 'User Not Found.';
+		} else if (user.hashed_password === hashPW(req.body.password.toString())) {
+			req.session.regenerate(function() {
+				req.session.user = user.id;
+				req.session.username = user.username;
+				req.session.msg = 'Authenticated as ' + user.username;
+				res.redirect('/');
+			});
+		} else {
+			err = 'Authentication failed.';
+		}
+		if (err) {
+			req.session.regenerate(function(){
+				req.session.msg = err;
+				res.redirect('/login');
+			});
+		}
+	});
+};
+exports.getUserProfile = function(req, res) {
+	User.findOne({_id: req.session.user})
+	.exec(function(err, user) {
+		if (!user) {
+			res.json(404, {err: 'User Not Found.'});
+		} else {
+			res.json(user);
+		}
+	});
+};
+exports.updateUser = function(req, res) {
+	User.findOne({_id: req.session.user})
+	.exec(function(err, user) {
+		user.set('email', req.body.email);
+		user.set('color', req.body.color);
+		user.save(function(err) {
+			if (err) {
+				res.session.error = err; /*HUOM tod näk typo, muoto nyt sama kuin kirjassa*/
+			} else {
+				req.session.msg = 'User Updated.';
+			}
+			res.redirect('/user');
+		});
+	});
+};
+exports.deleteUder = function(req, res) {
+	User.findOne({_id: req.session.user})
+	.exec(function(err, user) {
+		if (user) {
+			user.remove(function(err) {
+				if (err) {
+					req.session.msg = err;
+				}
+				req.session.destroy(function() {
+					res.redirect('/login');
+				});
+			});
+		} else {
+			req.session.msg = "User Not Found!";
+			req.session.destroy(function() {
+				res.redirect('/login');
+			});
 		}
 	});
 };
